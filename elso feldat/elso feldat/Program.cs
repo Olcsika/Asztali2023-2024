1d128b0cc96b20c6e7447e46517501f61a178d87
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2024-09-03 12:06:05 +0200

    a

---
+++ elso feldat/elso feldat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elso_feldat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter iras = new StreamWriter("ize.txt");
+
+            while (true)
+            {
+                
+                Console.WriteLine("Kérek egy számot");
+                int szam = Convert.ToInt32(Console.ReadLine());
+                iras.WriteLine(szam+" "+szam*szam);
+                if (szam == 0) 
+                {
+                    break;
+                    
+                }
+                
+
+            }
+            iras.Close();
+        }
+    }
+}
