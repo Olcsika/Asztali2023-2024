6d279abb737a386b939e0bd7e71260b107cdbeb8
Lébár Olivér <lebaro21@ady-nagyatad.sulinet.hu>
2024-09-04 12:05:17 +0200

    a

---
+++ nevek 100 ig/nevek 100 ig/Program.cs
@@ -0,0 +1,12 @@
+﻿StreamReader olvas = new StreamReader("nevek.txt");
+string[] vag =olvas.ReadToEnd().Split(',');
+Array.Sort(vag);
+
+olvas.Close();
+/*
+foreach (string i in vag)
+{
+    Console.WriteLine(i);
+}
+*/
+Console.WriteLine(vag[0]);
